9c47468cca38443181a15c2ddcfbab3b46157200
guojingwen <guojingwen@ksjgs.com>
2020-03-23 05:06:18 +0800

    feat:

---
+++ src/modules/app/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './style.css';
+
+export default class App extends React.Component {
+    
+    componentDidMount() {
+        navigator.mediaDevices.getUserMedia({
+            video: true,
+            // audio: true
+        })
+            .then(this.gotLocalMediaStream)
+            .catch(this.handleLocalMediaStreamError);
+    }
+    videoRef = null;
+
+    /**
+     * @desc    获取用户权限成功
+     * @param   {Object}    mediaStream     音视频轨
+     */
+    gotLocalMediaStream = (mediaStream) => {
+        console.log(mediaStream);
+        this.videoRef.srcObject = mediaStream;
+    }
+    handleLocalMediaStreamError = (error) => {
+        console.log('navigator.getUserMedia error: ', error);
+    }
+
+    render() {
+        return (
+            <div className="app-page">
+                video
+                <video
+                    className="video-content"
+                    ref={(video) => {
+                        this.videoRef = video;
+                    }}
+                    autoPlay
+                    playsInline
+                />
+            </div>
+        )
+    }
+}
